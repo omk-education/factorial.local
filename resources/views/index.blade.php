c5554631f675bb52fd0959cdd162ed6e77c71d67
Leonov Ivan <it.omk@yandex.ru>
2021-03-17 10:48:49 +0300

    1.5. Создание базового представления приложения

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Factorial</title>
+</head>
+<body>
+
+<!-- Вывод результата -->
+
+<!-- Форма для передачи данных -->
+
+</body>
+</html>
