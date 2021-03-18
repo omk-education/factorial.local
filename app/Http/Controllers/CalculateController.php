2fe48912fdcac1d3d0ab0ac0b9b0f83861e5dde7
Leonov Ivan <it.omk@yandex.ru>
2021-03-18 09:01:09 +0300

    1.7. Метод для вычисления факториала

--- app/Http/Controllers/CalculateController.php
+++ app/Http/Controllers/CalculateController.php
@@ -15,4 +15,34 @@ class CalculateController extends Controller
     {
         return view('index');
     }
+    
+    /**
+     * Вычисление факториала
+     *
+     * @param Request $request получение данных из формы
+     *
+     * @return View представление
+     */
+    public function factorial(Request $request)
+    {
+        // получение данных
+        $number = $request->number;
+
+        // валидация
+        $validated = $request->validate([
+            'number' => 'required|integer|min:0',
+        ]);
+
+        // вычисление факториала
+        $factorial = 1;
+        for ($i = 1; $i <= $number; $i++) {
+            $factorial *= $i;
+        }
+
+        // передача данных представлению
+        return view('index', [
+            'number' => $number,
+            'factorial' => $factorial,
+        ]);
+    }
 }
