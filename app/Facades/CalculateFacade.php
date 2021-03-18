c86a8e21a1806a3a76c54c71b5a3a39295aea50a
Leonov Ivan <it.omk@yandex.ru>
2021-03-18 10:14:07 +0300

    2.6. Создание фасада

---
+++ app/Facades/CalculateFacade.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class CalculateFacade extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'Calculate';
+    }
+}
